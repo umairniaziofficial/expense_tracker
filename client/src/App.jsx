6a6f20198f5249cd456c79c0407cb9f8e1b27b6d
umairniaziofficial <umairniaziofficial@gmail.com>
2024-06-16 04:03:56 +0500

    Project Completed All the Features are working without any errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Form from "./components/Form.jsx"
 
 export default function App() {
   return (
-    <div className="App font-semibold">
+    <div className="App font-semibold py-12">
       <div className="main">
         <div className="mx-auto text-center max-w-6xl drop-shadow-lg text-gray-800">
           <h1 className="text-4xl py-6 bg-slate-800  text-white rounded">Expense Tracker</h1>
